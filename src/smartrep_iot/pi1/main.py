51568ca765a0b7c415aa0f013aadbd6e457e0969
Victor Bien <victor.bien@gmail.com>
2026-04-28 11:33:25 +1200

    Refactor main.py for equipment state handling

--- src/smartrep_iot/pi1/main.py
+++ src/smartrep_iot/pi1/main.py
@@ -1,36 +1,41 @@
 import time
 import RPi.GPIO as GPIO
 
-from config import EQUIPMENT
+from config import EQUIPMENT, CHAIR, AVAILABLE, OCCUPIED, THRESHOLD, CHANNEL, SLEEP_TIME
 from adc import read_adc
 from led import setup_leds, update_led
 from session_manager import SessionManager
-#from mqtt_client import publish
+from mqtt_client import publish
 
 # Setup
 setup_leds(EQUIPMENT)
-#session_mgr = SessionManager(EQUIPMENT)
+session_mgr = SessionManager(EQUIPMENT)
 
 try:
     while True:
         overall_state = {}
 
         for name, eq in EQUIPMENT.items():
-            value = read_adc(eq["channel"])
-            occupied = value > eq["threshold"]
+            
+            value = read_adc(eq[CHANNEL])
+            
+            if name == CHAIR:
+                available = value < eq[THRESHOLD]
+            else:
+                available = value > eq[THRESHOLD]
 
-            update_led(eq, occupied)
-            session_mgr.handle(name, occupied)
+            update_led(eq, available)
+            session_mgr.handle(name, available)
 
-            overall_state[name] = "occupied" if occupied else "available"
+            overall_state[name] = AVAILABLE if available else OCCUPIED
 
-            print(f"{name}: {value}")
+            print(f"{name}: {overall_state[name]}")
 
         # Optional: publish all states together
-        #publish(overall_state)
-
+        publish(overall_state)
         print("-" * 30)
-        time.sleep(0.5)
+        time.sleep(SLEEP_TIME)
 
 finally:
     GPIO.cleanup()
+
