623b9ae7cb7024ae65ba5474c2f8adf3c4924b9e
Victor Bien <victor.bien@gmail.com>
2026-04-28 11:34:31 +1200

    Add equipment constants and update equipment mapping

--- src/smartrep_iot/pi1/config.py
+++ src/smartrep_iot/pi1/config.py
@@ -4,14 +4,43 @@ TB_PI1_TOKEN="Nyj11kX9rElJ7sUGhvDO"
 TB_PI2_TOKEN="gmvlMY1g7xVRYlfDQvKQ"
 TB_BROKER="mqtt.thingsboard.cloud"
 TB_PORT=1883
+MQTT_TOPIC = "v1/devices/me/telemetry"
+
+#Equipment Name
+DUMBBELL_LEFT = "dumbbell_left"
+DUMBBELL_RIGHT = "dumbbell_right"
+FOAM_ROLLER = "foam_roller"
+CHAIR = "chair"
+
+#Equipment Attributes
+CHANNEL = "channel"
+THRESHOLD = "threshold"
+LED_GREEN = "led_green"
+LED_RED = "led_red"
+
+#Equipment Status
+AVAILABLE = "available"
+OCCUPIED = "occupied"
+
+#Session Variables
+EVENT = "event"
+EQUIPMENT_NAME = "equipment"
+SESSION_ID = "session_id"
+START_TIME = "start_time"
+END_TIME = "end_time"
+SESSION_DURATION = "session_duration_s"
 
 # PCF8591
 PCF_ADDRESS = 0x48
 
+#Sleep Time
+SLEEP_TIME = 0.5
+
 # Equipment mapping
 EQUIPMENT = {
-    "dumbbell_left": {"channel": 0, "threshold": 100, "led_green": 18, "led_red": 17},
-    "dumbbell_right": {"channel": 1, "threshold": 100, "led_green": 27, "led_red": 22},
-    "foam_roller": {"channel": 2, "threshold": 90, "led_green": 24, "led_red": 23},
-    "chair": {"channel": 3, "threshold": 110, "led_green": 5, "led_red": 6},
+    DUMBBELL_LEFT: {CHANNEL: 0, THRESHOLD: 100, LED_GREEN: 18, LED_RED: 17},
+    DUMBBELL_RIGHT: {CHANNEL: 1, THRESHOLD: 100, LED_GREEN: 27, LED_RED: 22},
+    FOAM_ROLLER: {CHANNEL: 2, THRESHOLD: 100, LED_GREEN: 24, LED_RED: 23},
+    CHAIR: {CHANNEL: 3, THRESHOLD: 100, LED_GREEN: 5, LED_RED: 6},
 }
+
