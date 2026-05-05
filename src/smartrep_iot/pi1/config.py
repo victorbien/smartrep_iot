f7c96c7289410055af140b5e05e267acf9516330
Victor Bien <victor.bien@gmail.com>
2026-05-05 14:19:23 +1200

    Add DUMBBELL_PAIR constant to config.py

--- src/smartrep_iot/pi1/config.py
+++ src/smartrep_iot/pi1/config.py
@@ -9,6 +9,7 @@ MQTT_TOPIC = "v1/devices/me/telemetry"
 #Equipment Name
 DUMBBELL_LEFT = "dumbbell_left"
 DUMBBELL_RIGHT = "dumbbell_right"
+DUMBBELL_PAIR = "dumbbell_pair"
 FOAM_ROLLER = "foam_roller"
 CHAIR = "chair"
 
