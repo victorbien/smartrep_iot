4d62899b599f39f7dffe1509d3adb8cb34857f71
Victor Bien <victor.bien@gmail.com>
2026-05-05 14:17:45 +1200

    Update adc.py

--- src/smartrep_iot/pi1/adc.py
+++ src/smartrep_iot/pi1/adc.py
@@ -1,9 +1,10 @@
-import smbus
+import smbus2
 from config import PCF_ADDRESS
 
-bus = smbus.SMBus(1)
+bus = smbus2.SMBus(1)
 
 def read_adc(channel):
     bus.write_byte(PCF_ADDRESS, 0x40 | channel)
     bus.read_byte(PCF_ADDRESS)  # dummy read
     return bus.read_byte(PCF_ADDRESS)
+
